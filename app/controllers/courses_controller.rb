07e01928a648dcac44695bd293880dd1c990b13d
carlosmpr <carlosmpr0508@gmail.com>
2021-12-01 10:37:50 -0500

    Added the Record not found method

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,20 +1,19 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :update, :destroy]
+  rescue_from ::ActiveRecord::RecordNotFound, with: :record_not_found
 
+#Method to show all courses#
   def index
     @courses = Course.all
     render json: @courses
   end
 
-
+#Method to show specific course#
   def show
-    if @course
       render json: @course
-    else 
-      render json:{"error":"not found"}, status: :not_found
-    end
   end
 
+  #Method to create a course#
   def create
     @course = Course.new(course_params)
     if @course.save
@@ -24,7 +23,7 @@ class CoursesController < ApplicationController
     end
   end
 
-
+  #Method to update a course#
   def update
     if @course.update(course_params)
       render json: @course
@@ -33,18 +32,27 @@ class CoursesController < ApplicationController
     end
   end
 
+
+#Method to delete a specific course#
   def destroy
     @course.destroy
   end
 
+
   private
- 
+
+  #Method that set the global variable to the specific course for the show, destroy, or update method#
     def set_course
       @course = Course.find(params[:id])
     end
 
-   
+  #Method to handle the no record found#
+    def record_not_found(exception)
+      render json: {error: "No course found"}, status: :not_found
+    end
+
+    #Method to allow specific params#
     def course_params
-      params.require(:course).permit(:name, :d)
+      params.require(:course).permit(:name)
     end
 end
